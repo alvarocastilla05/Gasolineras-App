e14c0e9a1a378fa678e8da625c631984b79cbacb
alvarocastilla05 <castilla.caalv24@triana.salesianos.edu>
2024-11-05 10:59:09 +0100

    Cosas

--- AppGasolineras/src/app/components/gasolinera/gasolinera.component.ts
+++ AppGasolineras/src/app/components/gasolinera/gasolinera.component.ts
@@ -11,41 +11,7 @@ export class GasolineraComponent implements OnInit {
 
   listadoGasolineras: Gasolinera[] = []
 
-  /*
-  gasolinera: Gasolinera = {
-    "C.P.": "41400",
-    Dirección: "AVENIDA DE LA CONSTITUCION, 1",
-    Horario: "L-D: 24H",
-    Latitud: "37.582222",
-    Localidad: "ECIJA",
-    "Longitud (WGS84)": "-5.084167",
-    Margen: "D",
-    Municipio: "ÉCIJA",
-    "Precio Biodiesel": "0.000",
-    "Precio Bioetanol": "0.000",
-    "Precio Gas Natural Comprimido": "0.000",
-    "Precio Gas Natural Licuado": "0.000",
-    "Precio Gases licuados del petróleo": "0.000",
-    "Precio Gasoleo A": "1.159",
-    "Precio Gasoleo B": "0.000",
-    "Precio Gasoleo Premium": "0.000",
-    "Precio Gasolina 95 E10": "1.269",
-    "Precio Gasolina 95 E5": "1.269",
-    "Precio Gasolina 95 E5 Premium": "0.000",
-    "Precio Gasolina 98 E10": "0.000",
-    "Precio Gasolina 98 E5": "0.000",
-    "Precio Hidrogeno": "0.000",
-    Provincia: "SEVILLA",
-    Remisión: "DM",
-    Rótulo: "CEPSA",
-    "Tipo Venta": "P",
-    "% BioEtanol": "0.000",
-    "% Éster metílico": "0.000",
-    IDEESS: "ES0421",
-    IDMunicipio: "41310",
-    IDProvincia: "41",
-    IDCCAA: "01"
-  };*/
+
 
 
 
@@ -94,11 +60,9 @@ export class GasolineraComponent implements OnInit {
     return key;
   }
 
-  gasolineraParse(gasolinera: Gasolinera): Gasolinera {
-    const gasolineraString = JSON.stringify(gasolinera, this.replacer, 2);
-    return JSON.parse(gasolineraString);
+  filtrarPorRotulo(rotulo: string): Gasolinera[] {
+    return this.listadoGasolineras.filter(gasolinera => gasolinera.Rótulo === rotulo);
   }
-
 
 
 
