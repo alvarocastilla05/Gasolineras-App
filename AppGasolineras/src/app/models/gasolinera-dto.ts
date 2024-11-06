117e6379c8f01ffaf0b4276ed38b978024893749
Lucas Falla Urtiaga <falla.urluc24@triana.salesianos.edu>
2024-11-06 12:21:17 +0100

    Maquetación arreglada

--- AppGasolineras/src/app/models/gasolinera-dto.ts
+++ AppGasolineras/src/app/models/gasolinera-dto.ts
@@ -2,10 +2,11 @@ export class Gasolinera {
     constructor(
       public id: number,
       public nombre: string,
+      public direccion: string,
       public postalCode: string,
       public precioGasolina98E5: number,
       public precioGasoleoA: number,
       public precioHidrogreno: number,
-      
+      public precioBioetanol: number,
     ) {}
   }
